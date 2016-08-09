c267c7fdc0adea2a3b4b680d1c9b23d76478631f
yogendra kunwar <n16104@std.it-college.ac.jp>
2016-08-09 11:42:48 +0900

    8/10

--- timer.py
+++ timer.py
@@ -1,19 +1,19 @@
-import  time
+import time
 
 
-class timer(object):
-    def_init_(self, verbose=false):
-       self.verbose = verbose
+class Timer(object):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end_time = time.time()
+        self.secs = self.end_time - self.start_time
+        self.millis = self.secs * 1000
+        if self.verbose:
 
 
-    def_enter_(self):
-       self.start_time = time.time()
-       return self
-
-
-    def_exit_(self, exc_type,exc_val,exc_tb):
-       self.end = time.time()
-       self.secs = self.end_time - self.start_time
-       self.millis = self.secs * 1000
-       if self.verbose:
-           print('elapsed time: {0:f} ms'.format(self.mills))
+print('elasped time: {0:f} ms'.format(self.millis))
